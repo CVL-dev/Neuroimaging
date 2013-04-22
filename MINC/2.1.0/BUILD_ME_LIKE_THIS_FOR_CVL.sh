c586aef0d35c4cf4c2186d86d6e59b5785347365
Harald Waxenegger <harald.waxenegger@gmail.com>
2013-04-22 17:05:35 +1000

    added minc -- dependency for tissuestack

---
+++ MINC/2.1.0/BUILD_ME_LIKE_THIS_FOR_CVL.sh
@@ -0,0 +1,60 @@
+clear
+
+NAME=minc
+VERSION=2.1.0
+BUILD_DIR=`pwd`/minc_build
+
+DEST_PATH=${NAME^^}/$VERSION
+
+if [ `pwd | grep /usr/local/src/$DEST_PATH | wc -c` -eq 0 ]; then
+        echo "Please copy directory contents to '/usr/local/src/$DEST_PATH' and execute this script there!"
+        exit -1;
+fi
+
+tar xvzf $NAME-$VERSION-sources.tar.gz
+if [ $? -ne 0 ]; then
+        echo "Build aborted! Check for errors..."
+        exit -1
+fi
+
+echo -e "\n\n******************************************************************************************************"
+echo -e "!!! THIS BUILD OF MINC WILL ADD PERL MODULES, A CVL README.TXT AND TAR EVERYTHING !!!"
+echo -e "******************************************************************************************************\n\n"
+
+rm -rf $BUILD_DIR
+
+if [ ! -f /etc/profile.d/modules.sh ]; then echo "ERROR: Modules package is not installed !!!!!"; exit -1; fi;
+
+/etc/profile.d/modules.sh
+module load hdf5/1.8.5 2> /tmp/build_mod_load
+CHECK_SIZE=`stat -c%s /tmp/build_mod_load`
+if [ $CHECK_SIZE -ne 0 ]; then echo "ERROR: Could not locate needed hdf5 package (1.8.5). Please install it and load it: 'module load hdf5/1.8.5' !!!!!"; exit -1
+fi
+
+module load netcdf/4.1.1 2> /tmp/build_mod_load
+CHECK_SIZE=`stat -c%s /tmp/build_mod_load`
+if [ $CHECK_SIZE -ne 0 ]; then
+	echo "ERROR: Could not locate needed netcdf package (4.1.1). Please install it and load it: 'module load netcdf/4.1.1' !!!!!"; exit -1
+fi
+
+if [ ! -f /usr/include/curl/curl.h ]; then echo "ERROR: Could not find libcurl development headers. Insure the package is installed, e.g. run: 'yum install libcurl-devel"; exit -1
+fi 
+
+./configure --prefix=$BUILD_DIR/$NAME/$VERSION --with-build-path="/usr/local/netcdf/4.1.1 /usr/local/hdf5/1.8.5" && make install && make clean
+if [ $? -ne 0 ]; then
+        echo "Build aborted! Check for errors..."
+        exit -1
+fi
+
+cp -r usr $BUILD_DIR/$NAME/$VERSION
+rm -rf $NAME-$VERSION-sources.tar.gz
+
+echo "This is a binary build" > $BUILD_DIR/$NAME/$VERSION/readme.txt
+
+rm -rf $NAME-$VERSION-binaries.tar.gz
+tar cvfz $NAME-$VERSION-binaries.tar.gz $BUILD_DIR/$NAME
+rm -rf $BUILD_DIR
+
+echo -e "\n\n*******************************************************************"
+echo -e "\tNOW GO AHEAD AND CVL BUILD ME FROM THE TAR JUST CREATED"
+echo -e "*******************************************************************\n\n"
