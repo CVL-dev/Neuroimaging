f7e69411a07073399135543979534eb9a79aad28
Harald Waxenegger <harald.waxenegger@gmail.com>
2014-05-06 10:51:03 +1000

    added nimrod/g 3.4

---
+++ NIMROD/3.4/BUILD_ME_LIKE_THIS_FOR_CVL.sh
@@ -0,0 +1,49 @@
+clear
+
+NAME=nimrod
+VERSION=3.4
+BUILD_DIR=`pwd`/nimrod_build
+
+DEST_PATH=${NAME^^}/$VERSION
+
+if [ `pwd | grep /usr/local/src/$DEST_PATH | wc -c` -eq 0 ]; then
+        echo "Please copy directory contents to '/usr/local/src/$DEST_PATH' and execute this script there!"
+        exit -1;
+fi
+
+tar xvzf $NAME-$VERSION-sources.tar.gz
+if [ $? -ne 0 ]; then
+        echo "WARNING: $NAME-$VERSION-sources.tar.gz IS NOT PRESENT! If this is the first run of the script, the build WILL fail later on. If it isn't, you can safely ignore this message."
+fi
+
+echo -e "\n\n************************************************************************"
+echo -e "!!! THIS BUILD OF NIMROD IS TAILORED TOWARDS CVL BUILDING !!!"
+echo -e "*************************************************************************\n\n"
+
+rm -rf $BUILD_DIR
+if [ ! -f /etc/profile.d/modules.sh ]; then echo "ERROR: Modules package is not installed !!!!!"; exit -1; fi;
+mkdir -p $BUILD_DIR
+
+# build
+export PYTHONPATH=/usr/local/$NAME/$VERSION/share/$NAME
+./configure --prefix=/usr/local/$NAME/$VERSION && make && make install
+if [ $? -ne 0 ]; then
+        echo "Build aborted! Check for errors..."
+        exit -1
+fi
+rm -rf $NAME-$VERSION-sources.tar.gz
+
+# copy over into build dir (needed coz silly nimrod uses prefix to set runtime config instead of more flexible mechanisms) 
+cp -r /usr/local/$NAME $BUILD_DIR
+rm -rf /usr/local/$NAME
+
+echo "This is a binary build" > $BUILD_DIR/$NAME/$VERSION/readme.txt
+
+rm -rf $NAME-$VERSION-sources.tar.gz
+rm -rf $NAME-$VERSION-binaries.tar.gz
+tar cvfz $NAME-$VERSION-binaries.tar.gz $BUILD_DIR/$NAME
+rm -rf $BUILD_DIR
+
+echo -e "\n\n*******************************************************************"
+echo -e "\tNOW GO AHEAD AND CVL BUILD ME FROM THE TAR JUST CREATED"
+echo -e "*******************************************************************\n\n"
---
+++ NIMROD/3.4/post-install.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+# create log
+rm -f /tmp/post-install.log
+touch /tmp/post-install.log
+chmod 666 /tmp/post-install.log
+
+# add nimrod user
+useradd -c "nimrod" -m -d /home/nimrod -s /bin/bash -U nimrod &>> /tmp/post-install.log
+
+# init postgresql db (in case)
+su -c "su - postgres <<EOF
+initdb &>> /tmp/post-install.log
+EOF
+"
+
+# start postgresql
+chkconfig postgresql on &>> /tmp/post-install.log
+service postgresql start &>> /tmp/post-install.log
+sleep 5s
+
+# create nimrod database and assign it to nimrod user
+rm -rf /tmp/db_create.sql
+echo "CREATE USER nimrod WITH PASSWORD 'nimrod';" > /tmp/db_create.sql
+echo "CREATE DATABASE nimrod WITH OWNER nimrod;" >> /tmp/db_create.sql
+chown postgres:postgres /tmp/db_create.sql &>> /tmp/post-install.log
+
+su -c "su - postgres <<EOF
+psql -f /tmp/db_create.sql &>> /tmp/post-install.log
+EOF
+"
+rm -rf /tmp/db_create.sql &>> /tmp/post-install.log
+
+# run db objects creation scripts as nimrod
+echo "CREATE LANGUAGE plpgsql;" > /tmp/db_create_lang.sql
+chown nimrod:nimrod /tmp/db_create_lang.sql &>> /tmp/post-install.log
+su -c "su - nimrod <<EOF
+psql -f /tmp/db_create_lang.sql &>> /tmp/post-install.log
+rm -rf /tmp/db_create_lang.sql &>> /tmp/post-install.log
+source /etc/profile.d/nimrod_modules.sh &>> /tmp/post-install.log
+nimrod dbcreate &>> /tmp/post-install.log
+EOF
+"
+
+# open ports (optional)
+#if [ `iptables -S | grep -e "-A INPUT -i lo -j ACCEPT" | wc -c` -eq 0 ]; then
+#	iptables -I INPUT 1 -i lo -p all -j ACCEPT &>> /tmp/post-install.log
+#fi
+#if [ `iptables -S | grep -e "-A INPUT -p tcp -m tcp --dport 8080 -j DROP" | wc -c` -eq 0 ]; then
+#	iptables -A INPUT -p tcp --destination-port 8080 -j DROP &>> /tmp/post-install.log
+#fi
+#iptables-save &>> /tmp/post-install.log
