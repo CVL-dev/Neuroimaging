9148f958710261691e263e87884bac7ec2495f3c
waxenegger <harald.waxenegger@gmail.com>
2014-08-26 03:52:02 +0000

    updated nimrod version and added kepler plugin

---
+++ NIMROD_K/2.0.0/etc/profile.d/nimrod_g_modules.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo -n "Checking NIMROD G 'modules' requirements..."
+
+# set paths
+export NIMRODJAVAAPI_INSTALL=/usr/local/kepler/2.4/lib/nimrodk-2.4.0
+export KEPLER=/usr/local/kepler/2.4/lib
+
+echo -e "Finished.\n"
---
+++ NIMROD_K/2.0.0/post-install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# create log
+rm -f /tmp/post-install.log
+touch /tmp/post-install.log
+chmod 666 /tmp/post-install.log
+
+java -jar /usr/local/nimrod_k/2.0.0/nimrodk-2.0.0.jar -a --prefix=/usr/local/kepler/2.4/lib >> /tmp/post-install.log
