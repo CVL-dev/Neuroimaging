b87a9c9e0218d17ef94babe1a3a9c580b63be2e1
waxenegger <harald.waxenegger@gmail.com>
2014-08-29 00:54:01 +0000

    Nimrod Kepler plugin updates

--- NIMROD_K/2.0.0/etc/profile.d/nimrod_g_modules.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo -n "Checking NIMROD G 'modules' requirements..."
-
-# set paths
-export NIMRODJAVAAPI_INSTALL=/usr/local/kepler/2.4/lib/nimrodk-2.4.0
-export KEPLER=/usr/local/kepler/2.4/lib
-
-echo -e "Finished.\n"
