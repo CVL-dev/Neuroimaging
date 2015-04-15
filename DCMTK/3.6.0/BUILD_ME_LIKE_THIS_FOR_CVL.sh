8df1d23384038f809c4a66062a7cd790fcb34d34
Harald Waxenegger <harald.waxenegger@gmail.com>
2015-04-15 13:39:33 +1000

    updated dcmtk to include shared libs and headers

--- DCMTK/3.6.0/BUILD_ME_LIKE_THIS_FOR_CVL.sh
+++ DCMTK/3.6.0/BUILD_ME_LIKE_THIS_FOR_CVL.sh
@@ -26,10 +26,25 @@ mkdir -p ./etc/profile.d
 rm -rf config
 cp -r config.build config
 
-./configure --prefix=$BUILD_DIR/$NAME/$VERSION && make install && make clean
+./configure --prefix=$BUILD_DIR/$NAME/$VERSION
+if [ ! -f config/Makefile.def ]; then
+        echo "Configure must have failed. There is no config/Makefile.def"
+        exit 1
+fi
+
+#replacement to affect build of shared libs
+sed -i "/^AR[ ]*=/c\AR = gcc " config/Makefile.def
+sed -i "/^ARFLAGS[ ]*=/c\ARFLAGS = -shared -o " config/Makefile.def
+sed -i "/^LIBEXT[ ]*=/c\LIBEXT = so " config/Makefile.def
+sed -i "/^RANLIB[ ]*=/c\RANLIB = : " config/Makefile.def
+sed -i "/^CXXFLAGS[ ]*=/s/$/ -fPIC/" config/Makefile.def
+sed -i "/^CFLAGS[ ]*=/s/$/ -fPIC/" config/Makefile.def
+sed -i "/^LIBS[ ]*=/s/$/ -lpng/" config/Makefile.def
+
+make install && make install-lib
 if [ $? -ne 0 ]; then
-        echo "Build aborted! Check for errors..."
-        exit -1
+        echo "make/install failed"
+        exit 1
 fi
 
 echo "This is a binary build" > $BUILD_DIR/$NAME/$VERSION/readme.txt
