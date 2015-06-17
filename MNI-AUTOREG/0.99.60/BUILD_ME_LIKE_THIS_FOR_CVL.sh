11339b079ae6af1d365a30354545a20de967f97b
Harald Waxenegger <harald.waxenegger@gmail.com>
2015-06-17 11:00:33 +1000

    removed profile script

--- MNI-AUTOREG/0.99.60/BUILD_ME_LIKE_THIS_FOR_CVL.sh
+++ MNI-AUTOREG/0.99.60/BUILD_ME_LIKE_THIS_FOR_CVL.sh
@@ -52,28 +52,6 @@ rm -rf $NAME-$VERSION-sources.tar.gz
 
 echo "This is a binary build" > $BUILD_DIR/$NAME/$VERSION/readme.txt
 
-mkdir -p ./etc/profile.d
-rm -rf ./etc/profile.d/mni_autoreg_modules.sh
-cat > ./etc/profile.d/mni_autoreg_modules.sh <<EOF
-#!/bin/bash
-echo -n "Checking MNI-AUTOREG 'modules' requirements..."
-if [ ! -f /etc/profile.d/modules.sh ]; then echo -e "FAILED\nERROR: Modules package is not installed !!!!!\n"; fi;
-
-. /etc/profile.d/modules.sh
-
-if [ ! -f /tmp/build_mod_load ]; then touch /tmp/build_mod_load; chmod 777 /tmp/build_mod_load; fi;
-
-module load mni-autoreg/0.99.60 2> /tmp/build_mod_load
-CHECK_SIZE=\`stat -c%s /tmp/build_mod_load\`
-if [ \$CHECK_SIZE -ne 0 ]; then echo -e "FAILED\nERROR: Could not locate mni-autoreg package. Please install it and load it: 'module load mni-autoreg' !!!!!\n"
-fi
-
-rm -rf /tmp/build_mod_load
-
-echo -e "Finished.\n"
-EOF
-chmod 777 ./etc/profile.d/mni_autoreg_modules.sh
-
 rm -rf $NAME-$VERSION-binaries.tar.gz
 tar cvfz $NAME-$VERSION-binaries.tar.gz $BUILD_DIR/$NAME
 rm -rf $BUILD_DIR
